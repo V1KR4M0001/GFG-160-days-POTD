78facf9b27e21044b85220d613fd4c0e2f0fe9ae
V1KR4M0001 <168344287+V1KR4M0001@users.noreply.github.com>
2024-12-12 18:27:44 +0530

    Create Number of occurrence.cpp

---
+++ Number of occurrence.cpp
@@ -0,0 +1,82 @@
+  PROBLEM STATEMENT :->
+// Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+
+// Examples :
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+// Output: 4
+// Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+// Output: 0
+// Explanation: target = 4 is not present in the given array so the output is 0.
+
+// Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+// Output: 3
+// Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+
+
+// Constraints:
+// 1 ≤ arr.size() ≤ 106
+// 1 ≤ arr[i] ≤ 106
+// 1 ≤ target ≤ 106
+
+  PROBLEM LINK :->  https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/number-of-occurrence2259
+
+  SOLUTION :->
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        int count = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] == target) count++;
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countFreq(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
