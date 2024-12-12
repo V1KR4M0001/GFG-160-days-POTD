80276658f41ca9adc9017fdeba6d940b6030bc2f
V1KR4M0001 <168344287+V1KR4M0001@users.noreply.github.com>
2024-12-12 18:37:54 +0530

    Update Number of occurrence.cpp

--- Number of occurrence.cpp
+++ Number of occurrence.cpp
@@ -34,13 +34,50 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    // Bruite Force
+    // int countFreq(vector<int>& arr, int target) {
+    //     // code here
+    //     int count = 0;
+    //     for(int i = 0; i < arr.size(); i++) {
+    //         if(arr[i] == target) count++;
+    //     }
+    //     return count;
+    // }
+
+    // Optimized Code
+    
+    int firstOcc(vector<int>& arr, int target) {
+        int s = 0, n = arr.size() - 1;
+        int ans = -1;
+        while(s <= n) {
+            int mid = (s+n) / 2;
+            if(arr[mid] == target) {
+                ans = mid;
+                n = mid-1;
+            }
+            else if(arr[mid] > target) n = mid-1;
+            else s = mid+1;
+        }
+        return ans;
+    }
+    int lastOcc(vector<int>& arr, int target) {
+        int s = 0, n = arr.size()-1;
+        int ans = -1;
+        while(s <= n) {
+            int mid = (s+n) / 2;
+            if(arr[mid] == target) {
+                ans = mid;
+                s = mid+1;
+            }
+            else if(arr[mid] > target) n= mid-1;
+            else s = mid+1;
+        }
+        return ans;
+    }
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int count = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] == target) count++;
-        }
-        return count;
+        if(lastOcc(arr, target)==-1 && firstOcc(arr, target)==-1) return 0;
+        return lastOcc(arr, target)-firstOcc(arr, target) +1;
     }
 };
 
