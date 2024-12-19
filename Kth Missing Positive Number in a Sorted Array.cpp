ee60c6edd20370897ea13d6d3120a84206158a86
V1KR4M0001 <168344287+V1KR4M0001@users.noreply.github.com>
2024-12-19 19:52:37 +0530

    Create Kth Missing Positive Number in a Sorted Array.cpp

---
+++ Kth Missing Positive Number in a Sorted Array.cpp
@@ -0,0 +1,86 @@
+  PROBLEM STATEMENT :->
+// Given a sorted array of distinct positive integers arr[], we need to find the kth positive number that is missing from arr[].  
+
+
+// Examples :
+// Input: arr[] = [2, 3, 4, 7, 11], k = 5
+// Output: 9
+// Explanation: Missing are 1, 5, 6, 8, 9, 10… and 5th missing number is 9.
+
+// Input: arr[] = [1, 2, 3], k = 2
+// Output: 5
+// Explanation: Missing are 4, 5, 6… and 2nd missing number is 5.
+
+// Input: arr[] = [3, 5, 9, 10, 11, 12], k = 2
+// Output: 2
+// Explanation: Missing are 1, 2, 4, 6… and 2nd missing number is 2.
+
+// Constraints:
+// 1 <= arr.size() <= 105
+// 1 <= k <= 105
+// 1 <= arr[i]<= 106
+
+  PROBLEM LINK :-> https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-gfg-160/problem/kth-missing-positive-number-in-a-sorted-array
+
+  SOLUTION :->
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int kthMissing(vector<int> &arr, int k) {
+        // Your code goes here
+        int s = 0;
+        int e = arr.size()-1;
+        while(s <= e) {
+            int mid = (s+e) / 2;
+            int missing = arr[mid] - mid - 1;
+            if(missing < k) s = mid+1;
+            else e = mid-1;
+        }
+        return k + s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.kthMissing(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
